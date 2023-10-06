1bcef8f467926724ae6f701c5c8174e5e60b547e
Gery Liam <77185808+gl79@users.noreply.github.com>
2023-10-06 20:46:49 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>Travel Umroh Amanah Terbaik dan Penuh Hikmah</title>
+    <meta content="Menyajikan Pelayanan Umroh dan Wisata Halal Dengan Pelayanan Terbaik" name="description" />
+    <meta content="Travel, Umroh, Haji" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="assets/img/logo/logo1.png" rel="icon" />
+    <link href="assets/img/logo/logo1.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- ======= Top Bar ======= -->
+    <section id="topbar" class="d-flex align-items-center">
+        <div class="container d-flex justify-content-center justify-content-md-between">
+            <div class="contact-info d-flex align-items-center">
+                <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">kautsarmotivatour@gmail.com</a></i>
+                <i class="bi bi-whatsapp d-flex align-items-center ms-4"><a href="https://wa.me/6281805900063?text=Assalamualaikum%20Admin,%20saya%20mau%20bergabung%20menjadi%20Agen/Mitra%20Travel%20Kautsar%20Motivatour" target="_blank"><span>WhatsApp</span></a></i>
+            </div>
+            <div class="social-links d-none d-md-flex align-items-center">
+                <a href="https://www.instagram.com/kautsarmotivatour/" target="_blank" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.tiktok.com/@kautsarmotivatour.com" target="_blank" class="tiktok"><i class="bi bi-tiktok"></i></a>
+                <a href="https://www.facebook.com/KautsarMotivatour/" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.youtube.com/@kautsarmotivatourtravel" target="_blank" class="youtube"><i class="bi bi-youtube"></i></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+            <a href="index.html" class="logo"><img src="assets/img/logo/logo2.png" alt="" /></a>
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto" href="index.html">Beranda</a></li>
+                    <li class="dropdown">
+                        <a href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li>
+                                <a class="nav-link scrollto" href="layananpaketumroh.html">Paket Umroh</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="layananpakethaji.html">Paket Haji Furoda</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="layananbadal.html">Badal Haji / Badal Umroh</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="#">Wisata Halal 3 Negara</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="#">Wisata Halal Turkiye Only</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#"><span>Angsuran Umroh</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li>
+                                <a class="nav-link scrollto" href="panel/">Angsuran Baru</a>
+                            </li>
+                            <li><a class="nav-link scrollto" href="panel/">Setoran</a></li>
+                            <li>
+                                <a class="nav-link scrollto" href="ceksetoran.php">Cek Setoran</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="#">Kemitraan</a></li>
+                    <li><a class="nav-link scrollto" href="blog.html">Berita</a></li>
+                    <li><a class="nav-link scrollto" href="faq.html">F.A.Q</a></li>
+                    <li>
+                        <a href="panel/" class="btn-get-started" role="button">Daftar / Login</a>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+            <!-- .navbar -->
+        </div>
+    </header>
+    <!-- End Header -->
+
+    <main id="main" data-aos="fade-up">
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact">
+            <div class="container" data-aos="fade-up">
+                <div class="section-title">
+                    <h2>Kontak</h2>
+                    <h3>Hubungi <span>Kautsar Motivatour</span></h3>
+                    <p>Perjalanan Hikmah Penuh Makna</p>
+                </div>
+
+                <div class="row" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-6">
+                        <div class="info-box mb-4">
+                            <i class="bx bx-map"></i>
+                            <h3>Alamat Kami</h3>
+                            <p>
+                                Jl. Lb. Bulus Pd. Ruko No.6C, Lb. Bulus, Kec. Cilandak, Kota
+                                Jakarta Selatan, <br />
+                                Daerah Khusus Ibukota Jakarta 12240
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="info-box mb-4">
+                            <i class="bx bx-envelope"></i>
+                            <h3>Email Kami</h3>
+                            <p>kautsarmotivatour@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="info-box mb-4">
+                            <i class="bx bx-phone-call"></i>
+                            <h3>Telepon</h3>
+                            <p>08119193998</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-6">
+                        <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.72693304516!2d106.7745279!3d-6.299567700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f105fd155659%3A0x5b6f80ae245f6e9f!2sKautsar%20Motivatour!5e0!3m2!1sen!2sid!4v1695051084992!5m2!1sen!2sid" frameborder="0" style="border: 0; width: 100%; height: 384px" allowfullscreen></iframe>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <form action="https://formsubmit.co/51fe7804bc87500c6359d82f599895a3" method="post" role="form" class="php-email-form">
+                            <!--Honeypot -->
+                            <input type="text" name="_honey" style="display: none;">
+                            <input type="hidden" name="_captcha" value="false">
+                            <div class="row">
+                                <div class="col form-group">
+                                    <input type="text" name="name" class="form-control" id="name" placeholder="Nama Anda" required />
+                                </div>
+                                <div class="col form-group">
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="Email Anda" required />
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Judul Pesan" required />
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" rows="5" placeholder="Pesan" required></textarea>
+                            </div>
+                            <div class="my-3">
+                                <div class="loading">Loading</div>
+                                <div class="error-message">Berhasil:</div>
+                                <div class="sent-message">
+                                    Your message has been sent. Thank you!
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit">Kirim Pesan</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3>Kautsar Motivatour<span></span></h3>
+                        <p>
+                            Jl. Lb. Bulus Pd. Ruko No.6C,<br />
+                            Lb. Bulus, Kec. Cilandak, Kota Jakarta Selatan,<br />
+                            Daerah Khusus Ibukota Jakarta 12240. <br />
+                            <strong>Telepon :</strong> 08119193998<br />
+                            <strong>Email :</strong> kautsarmotivatour@gmail.com<br />
+                            <strong>Jam Operasional : <br /></strong> Senin - Jumat : 09.00
+                            - 17.00<br />Sabtu : 09.00 - 14.00 <br />
+                            Ahad : Libur
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Link</h4>
+                        <ul>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="index.html">Beranda</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="index.html">Tentang Kami</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="panel/">Setoran</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="ceksetoran.php">Cek Setoran</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="faq.html">F.A.Q</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Layanan</h4>
+                        <ul>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="paketspesial.html">Umroh Spesial</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="paketplusaqsa.html">Umroh Plus Aqsa</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="kemitraan.html">Kemitraan</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="pakethemat.html">Umroh Super Hemat</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-chevron-right"></i>
+                                <a href="pakethealing.html">Umroh Healing</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Social Media</h4>
+                        <p>
+                            Follow & Subscribe Untuk Mendapatkan Informasi dan Promo Menarik
+                            Lainnya !
+                        </p>
+                        <div class="social-links mt-3">
+                            <a href="https://www.youtube.com/@kautsarmotivatourtravel/videos" target="_blank" class="youtube"><i class="bx bxl-youtube"></i></a>
+                            <a href="https://www.facebook.com/KautsarMotivatour/" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="https://www.instagram.com/kautsarmotivatour/" target="blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="https://www.tiktok.com/@kautsarmotivatour.com" target="_blank" class="tiktok"><i class="bx bxl-tiktok"></i></a>
+                            <a href="https://kautsarmotivatour.com/" target="_blank" class="website"><i class="bx bx-world"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container py-4">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Kautsar Motivatour</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                Created & Designed With <i class="bx bxs-heart"></i> Since 2023
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+    <a href="https://wa.me/6281805900063?text=Assalamualaikum%20Admin,%20saya%20mau%20bergabung%20menjadi%20Agen/Mitra%20Travel%20Kautsar%20Motivatour" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
